c3c5c27e98c39d907b16018b5c10383e406e5b2e
Joe Boulos <joeboulos@ZENN-MBP0164.local>
2023-09-27 12:08:02 -0700

    vertical allign two containers, width calc, add display inline-block to block elements for navbar

---
+++ terms.js
@@ -0,0 +1,14 @@
+/**Terms to remember
+ *
+ * specificity
+ * inheritance
+ * combinator
+ * Combinator types: Adjacent Sibling, General Sibling, Child, Descendant
+ * margin-collapsing
+ * BLOCK ELEMENTS
+ * INLINE-ELEMENTS
+ * Content-Box
+ * BOX SIZING
+ * DISPLAY: INLINE-BLOCK
+ *
+ */
